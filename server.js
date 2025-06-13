fb690c488d9b10ce724a7d1fb2b8a6a6672c196a
somerandom-cpu <somerandommail676@gmail.com>
2025-06-12 21:43:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
+  'https://meetcute-frontend.vercel.app',
 ];
 
 // Enable pre-flight across-the-board
